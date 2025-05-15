cddadbd44a57efeb22abee8a3fe24520876b7f44
owaisnadeem18 <owaisnadeem15@gmail.com>
2025-05-15 21:33:15 +0500

    Create Question-01.py

---
+++ All Practice Questions Of OOP (Basics)/Question-01.py
@@ -0,0 +1,16 @@
+# Create class Programmer for storing info of few programmers working at Microsoft: 
+
+class Student():
+    def __init__(self , id , dept , name):
+        self.id = id 
+        self.dept = dept
+        self.name = name
+
+    def student_Details(self):
+        print(f"The name of the student is '{self.name}' bearing the roll # '{self.id}' from dept. '{self.dept}'")
+
+student1 = Student(983 , "Owais Nadem" , "BSCS")
+student2 = Student( 4334,"Wifey aka Rabia Owais" , "Baatain banana or baatain sunana")
+
+student1.student_Details()
+student2.student_Details()
